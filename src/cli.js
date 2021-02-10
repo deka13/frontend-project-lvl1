7ce23d990a0a61e4140a64a7181e6801f63dc60f
Denis Klyuev <deka.klyuev@mail.ru>
2021-02-10 17:57:31 +0700

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,10 @@ import readlineSync from 'readline-sync';
 
 // eslint-disable-next-line import/prefer-default-export
 export const greeting = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   // eslint-disable-next-line no-console
   console.log(`Hello, ${userName}!`);
 };
+
+export default greeting();
