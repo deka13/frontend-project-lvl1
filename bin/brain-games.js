a01ffafc88a5f42c67cdf919885b6838a62f79f8
Denis Klyuev <deka.klyuev@mail.ru>
2021-02-10 17:58:20 +0700

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 // eslint-disable-next-line import/extensions
 import { greeting } from '../src/cli.js';
-// eslint-disable-next-line no-console
-console.log('Welcome to the Brain Games!');
+
 greeting();
