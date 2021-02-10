ab91301cb077dd30001fb4860e5334520eda1cdd
Denis Klyuev <deka.klyuev@mail.ru>
2021-02-10 17:48:10 +0700

    Changed const name in cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 // eslint-disable-next-line import/prefer-default-export
 export const greeting = () => {
-  const name = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   // eslint-disable-next-line no-console
-  console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${userName}!`);
 };
