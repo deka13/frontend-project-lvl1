eda32362f94d5700fe51e51da84104634c8f1b0f
Denis Klyuev <deka.klyuev@mail.ru>
2021-02-10 17:40:23 +0700

    Add index.js with game basic function

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+const numbersOfRounds = 3;
+
+export const runGame = (task, generateData) => {
+  console.log('Welcome');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(task);
+  for (let i = 1; i <= numbersOfRounds; i += 1) {
+    const [answer, question] = generateData();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('You answer: ');
+    if (answer === userAnswer) {
+      console.log('Correct!');
+    }
+    if (answer !== userAnswer) {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".
+      Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export const getRandomInt = (min, max) => {
+  const minRange = Math.ceil(min);
+  const maxRange = Math.floor(max);
+  return Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
+};
