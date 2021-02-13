9610f92b716966469c58206caf6b409fc9df6a32
Denis Klyuev <deka.klyuev@mail.ru>
2021-02-13 22:31:12 +0700

    Add brain-gcd.js with game logic

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,23 @@
+import { runGame, getRandomInt } from '../index.js';
+
+const task = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (num1, num2) => {
+  if (num2 === 0) {
+    return num1;
+  }
+  return gcd(num2, num1 % num2);
+};
+
+const min = 3;
+const max = 100;
+
+const gameData = () => {
+  const num1 = getRandomInt(min, max);
+  const num2 = getRandomInt(min, max);
+  const question = `${num1} ${num2}`;
+  const answer = String(gcd(num1, num2));
+  return [answer, question];
+};
+
+export default () => runGame(task, gameData);
