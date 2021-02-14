b35368a539eb9084bc801ec82baf85654b7927b2
Denis Klyuev <deka.klyuev@mail.ru>
2021-02-14 13:36:24 +0700

    Add brain-gcd to with game logic

---
+++ src/games/brain-progression.js
@@ -0,0 +1,27 @@
+import { runGame, getRandomInt } from '../index.js';
+
+const task = 'What number is missing in the progression?';
+
+const progressionMin = 5;
+const progressionMax = 10;
+const minStep = 3;
+const maxStep = 9;
+const minFirstNum = 0;
+const maxFirstNum = 50;
+
+const makeProgression = () => {
+  const progressionLength = getRandomInt(progressionMin, progressionMax);
+  const step = getRandomInt(minStep, maxStep);
+  const firstNum = getRandomInt(minFirstNum, maxFirstNum);
+  const arr = [];
+  for (let i = firstNum; i < progressionLength; i += step) {
+    arr.push(i);
+  }
+  const randomIndex = getRandomInt(0, arr.length - 1);
+  const answer = String(arr[randomIndex]);
+  arr[randomIndex] = '..';
+  const question = `${arr.join(' ')}`;
+  return [answer, question];
+};
+
+export default () => runGame(task, makeProgression());
