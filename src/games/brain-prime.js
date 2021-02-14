192875491049f8ce06e48d6728a48dabe5c20fa5
Denis Klyuev <deka.klyuev@mail.ru>
2021-02-14 14:37:49 +0700

    Add src/games/brain-prime.js with game logic

---
+++ src/games/brain-prime.js
@@ -0,0 +1,23 @@
+import { runGame, getRandomInt } from '../index.js';
+
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const min = 1;
+const max = 71;
+
+const gameData = () => {
+  const question = getRandomInt(min, max);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return [answer, question];
+};
+
+export default () => runGame(task, gameData);
